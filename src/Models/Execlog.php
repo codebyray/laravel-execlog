36c5c5b5854fd85031975abd75b20048cc94bf48
CodebyRay <codebyray@gmail.com>
2018-09-05 21:24:44 -0700

    Modified model to include table name

--- src/Models/Execlog.php
+++ src/Models/Execlog.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Execlog extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'execlogs';
 
 }
