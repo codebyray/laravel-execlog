532803ec2707e9f5765f6f253876477e53e2145b
CodebyRay <codebyray@gmail.com>
2018-09-05 20:59:46 -0700

    Fixed naming issue in migration file

--- database/migrations/2018_09_05_093625_create_execlog_table.php
+++ database/migrations/2018_09_05_093625_create_execlog_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateExeclogsTable extends Migration
+class CreateExeclogTable extends Migration
 {
     /**
      * Run the migrations.
